9ed430a12fc1826d60e667a18537e89c9b3bab3c
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-01 13:54:53 +0000

    promise more

--- script.js
+++ script.js
@@ -200,3 +200,60 @@ evt.on( "completion", function(){
 evt.on( "failure", function(err){
 	// oops, something went wrong in `foo(..)`
 } );
+
+//Promise "Events"
+
+function foo(x) {
+	// start doing something that could take a while
+
+	// construct and return a promise
+	return new Promise( function(resolve,reject){
+		// eventually, call `resolve(..)` or `reject(..)`,
+		// which are the resolution callbacks for
+		// the promise.
+	} );
+}
+
+var p = foo( 42 );
+
+bar( p );
+
+baz( p );
+
+//internals of bar(..) and baz(..)
+
+function bar(fooPromise) {
+	// listen for `foo(..)` to complete
+	fooPromise.then(
+		function(){
+			// `foo(..)` has now finished, so
+			// do `bar(..)`'s task
+		},
+		function(){
+			// oops, something went wrong in `foo(..)`
+		}
+	);
+}
+
+// ditto for `baz(..)`
+
+//Another way to approach this is:
+
+function bar() {
+	// `foo(..)` has definitely finished, so
+	// do `bar(..)`'s task
+}
+
+function oopsBar() {
+	// oops, something went wrong in `foo(..)`,
+	// so `bar(..)` didn't run
+}
+
+// ditto for `baz()` and `oopsBaz()`
+
+var p = foo( 42 );
+
+p.then( bar, oopsBar );
+
+p.then( baz, oopsBaz );
+
