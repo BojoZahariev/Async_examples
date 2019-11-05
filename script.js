bf0d01b9352e465d360b67fc59a8fbdf023aaee3
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-05 14:18:48 +0000

    async await

--- script.js
+++ script.js
@@ -374,3 +374,30 @@ let promise = new Promise(function(resolve, reject) {
 		// do something asynchronously and return a promise
 		return result;
 	  }
+
+	anArray.forEach(async item => {
+		// do something asynchronously for each item in 'anArray'
+		// one could also use .map here to return an array of promises to use with 'Promise.all()'
+	});
+
+	server.getPeople().then(async people => {
+		people.forEach(person => {
+		  // do something asynchronously for each person
+		});
+	});
+
+	//Error Handling
+
+	asyncFunctionCall().catch(err => {
+		console.error(err)
+	});
+
+	async function getPersonsInfo(name) {
+		try {
+		  const people = await server.getPeople();
+		  const person = people.find(person => { return person.name === name });
+		  return person;
+		} catch (error) {
+		  // Handle the error any way you'd like
+		}
+	}
