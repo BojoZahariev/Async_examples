3822ad4b5f6eea9d538e37906a1b40c31d0ce21e
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-05 17:22:55 +0000

    async/ await

--- script.js
+++ script.js
@@ -401,3 +401,41 @@ let promise = new Promise(function(resolve, reject) {
 		  // Handle the error any way you'd like
 		}
 	}
+
+	async function f() {
+
+		let promise = new Promise((resolve, reject) => {
+		  setTimeout(() => resolve("done!"), 1000)
+		});
+	  
+		let result = await promise; // wait until the promise resolves (*)
+	  
+		alert(result); // "done!"
+	  }
+
+	  ////////////////
+	  async function showAvatar() {
+
+		// read our JSON
+		let response = await fetch('/article/promise-chaining/user.json');
+		let user = await response.json();
+	  
+		// read github user
+		let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+		let githubUser = await githubResponse.json();
+	  
+		// show the avatar
+		let img = document.createElement('img');
+		img.src = githubUser.avatar_url;
+		img.className = "promise-avatar-example";
+		document.body.append(img);
+	  
+		// wait 3 seconds
+		await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+	  
+		img.remove();
+	  
+		return githubUser;
+	  }
+	  
+	  showAvatar();
