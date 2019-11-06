15b28e5a48d7fe1d0f783ab17c4a27c9812b6415
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-06 15:48:58 +0000

    more examples

--- script.js
+++ script.js
@@ -520,6 +520,28 @@ let promise = new Promise(function(resolve, reject) {
 	  loadJson('no-such-user.json')
 		.catch(alert); // Error: 404 
 
-	////////////////
-	
+	////////////////////////////////////////////////////////////////////
+
+	function doubleAfter2Seconds(x) {
+		return new Promise(resolve => {
+		  setTimeout(() => {
+			resolve(x * 2);
+		  }, 2000);
+		});
+	  }
+	  
+	  async function addAsync(x) {
+		const a = await doubleAfter2Seconds(10);
+		const b = await doubleAfter2Seconds(20);
+		const c = await doubleAfter2Seconds(30);
+		return x + a + b + c;
+	  }
+	  
+	  
+	  addAsync(10).then((sum) => {
+		console.log(sum);
+	  });
+
+	  
+
 
